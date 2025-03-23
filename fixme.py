d70d0c7622d0c3410f197daf71ed4cc0f88425c6
ananyaa-04 <anyaa0219@gmail.com>
2025-03-23 17:35:07 +0530

    Fix Fibonacci series in python

--- fixme.py
+++ fixme.py
@@ -1,12 +1,13 @@
 def fibonacci_iterative(n):
     if n <= 0:
         return 0
-    elif n == 1: #Fixed 
+    elif n == 1: #Fixed
         return 1
     
     a, b = 0, 1
     for _ in range(2, n+1):
         a, b = b, a + b
+    return b # Fixed
 
 n = 10
 print(f"The {n}th Fibonacci number is: {fibonacci_iterative(n)}")
